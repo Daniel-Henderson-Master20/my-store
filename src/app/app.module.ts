28d851524a9d614192ee6fe5330afd6897363840
Daniel-Henderson-Master20 <danielhenderson85@gmail.com>
2021-11-10 23:45:52 -0500

    cleaned up folders and files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CartComponent } from './componentw/cart/cart.component';
-import { Cart } from './componentw/cart/cart.component/cart.component.component';
+import { CartComponent } from './component/cart/cart.component';
+import { Cart } from './component/cart/cart.component';
 import { CheckoutSuccess } from './component/checkout-success/checkout-success.component/checkout-success.component.component';
 import { CreateForm } from './component/create-form/create-form.component/create-form.component.component';
-import { Header } from './component/header/header.component/header.component.component';
-import { Navbar } from './component/navbar/navbar.component/navbar.component.component';
-import { ProductItemDetail } from './component/product-item-detail/product-item-detail.component/product-item-detail.component.component';
-import { ProductList } from './component/product-list/product-list.component/product-list.component.component';
-import { ProductList } from './component/product-list/product-list.compenent/product-list.compenent.component';
+import { Header } from './component/header/header.component/';
+import { Navbar } from './component/navbar/navbar.component';
+import { ProductItemDetail } from './component/product-item-detail/product-item-detail.component';
+import { ProductList } from './component/product-list/product-list.component';
+import { ProductList } from './component/product-list/product-list.compenent';
 
 @NgModule({
   declarations: [
     AppComponent,
     CartComponent,
-    Cart.ComponentComponent,
-    CheckoutSuccess.ComponentComponent,
-    CreateForm.ComponentComponent,
-    Header.ComponentComponent,
-    Navbar.ComponentComponent,
-    ProductItemDetail.ComponentComponent,
-    ProductList.ComponentComponent,
-    ProductList.CompenentComponent
+    CartComponent,
+    CheckoutSuccess.Component,
+    CreateForm.Component,
+    Header.Component,
+    Navbar.Component,
+    ProductItemDetail.Component,
+    ProductList.Component,
+    ProductList.Compenent
   ],
   imports: [
     BrowserModule,
